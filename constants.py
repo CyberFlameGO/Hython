ddbfa81f716c66395012b03f0d8c91156e8977fc
Eros <george.marr.cobc@gmail.com>
2018-08-14 19:34:30 +0100

    adds in constants

---
+++ constants.py
@@ -0,0 +1,4 @@
+API_KEY = '#####'  # REPLACE WITH YOUR API KEY
+API_URL = 'https://api.hypixel.net/'
+UUID_RESOLVER = 'https://sessionserver.mojang.com/session/minecraft/profile/'
+KEY_LENGTH = 36
---
+++ json_handler.py
@@ -0,0 +1,10 @@
+def get_json_data(requestType, **kwargs):
+    """
+    handles getting data from the api
+    :param requestType:
+    :param kwargs:
+    :return:
+    """
+    end = ''
+    if requestType == 'key':
+        
