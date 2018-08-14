4351f7ac0b7ec7bd652a519c4d4dd8c3823aaa96
Eros <george.marr.cobc@gmail.com>
2018-08-14 20:03:17 +0100

    finishes the json handling

--- json_handler.py
+++ json_handler.py
@@ -31,3 +31,19 @@ def get_json_data(request_type, **kwargs):
             name = uuid_type
         end = '&' + name + '=' + val
     all_url = c.API_URL + request_type + '?key=' + key + end
+
+    for u in all_url:
+        requests = g.get(u)
+        responses = g.imap(requests)
+
+        for r in responses:
+            response = r.json()
+            if response['success'] is False:
+                raise e.HypixelAPIException
+            if request_type == 'player':
+                if response['player'] is None:
+                    raise e.PlayerNotFoundException
+    try:
+        return response[request_type]
+    except e.HypixelAPIException:
+        return response
