a7c46d060c447a20896fa489e390cedda83d8d81
Eros <george.marr.cobc@gmail.com>
2018-08-14 19:39:32 +0100

    now we're getting somewhere

--- json_handler.py
+++ json_handler.py
@@ -2,6 +2,7 @@ import constants as c
 import random
 from exceptions import hypthon_exceptions as e
 
+
 def get_json_data(requestType, **kwargs):
     """
     handles getting data from the api
@@ -13,6 +14,14 @@ def get_json_data(requestType, **kwargs):
     try:
         if requestType == 'key':
             key = kwargs['key']
-
     except e.JsonParsingException and e.InvalidAPIKeyException:
+        pass
+
+    try:
+        if requestType == 'player':
+            uuid_type = 'uuid'
+            uuid = kwargs['uuid']
+            if len(uuid) <= 16:
+                uuid_type = 'name'
+    except e.JsonParsingException and e.PlayerNotFoundException:
         pass
