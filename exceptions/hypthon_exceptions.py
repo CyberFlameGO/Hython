e46207cc425a3664bc8b93870a9fd0daf83dc35b
Eros <george.marr.cobc@gmail.com>
2018-08-15 00:42:35 +0100

    pass through everything

--- exceptions/hypthon_exceptions.py
+++ exceptions/hypthon_exceptions.py
@@ -1,17 +1,23 @@
 class PlayerNotFoundException(Exception):
     print('An error returned while trying to find that player >> player not found! ')
+    pass
 
 
 class GuildNotFoundException(Exception):
     print('An error returned while trying to find that guild >> guild not valid!')
+    pass
 
 
 class HypixelAPIException(Exception):
     print('API has caught an error, this is not due to Hypthon!')
+    pass
 
 
 class InvalidAPIKeyException(Exception):
     print('Your API key is invalid, check it!')
+    pass
+
 
 class JsonParsingException(Exception):
     print('Error caught while attempting to parse JSON data from the API')
+    pass
