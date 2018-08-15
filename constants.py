c3fd92de2f156ebd02fec517d973caa325168887
Eros <george.marr.cobc@gmail.com>
2018-08-15 01:23:05 +0100

    adds a setter for the key

--- constants.py
+++ constants.py
@@ -2,3 +2,10 @@ API_KEY = '#####'  # REPLACE WITH YOUR API KEY
 API_URL = 'https://api.hypixel.net/'
 UUID_RESOLVER = 'https://sessionserver.mojang.com/session/minecraft/profile/'
 KEY_LENGTH = 36
+
+
+class Setter:
+
+    @staticmethod
+    def set_api_Key(key):
+        API_KEY = key
