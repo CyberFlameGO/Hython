8b8d2438ee0f0d179dcee28ced43b8fef68a9525
Eros <george.marr.cobc@gmail.com>
2018-09-19 10:18:03 +0100

    adds in player test

--- tests/test_player.py
+++ tests/test_player.py
@@ -4,3 +4,12 @@ from constants import Setter
 
 def handle_setup(key):
     Setter.set_api_Key(key)
+
+
+def handle_player_test(uuid):
+    uuid = 'should-replace-with-my-key'
+    Player.uuid = uuid
+    print(Player.find_level())
+    print(Player.get_player_info())
+    print(Player.get_rank())
+    print(Player.JSON())
