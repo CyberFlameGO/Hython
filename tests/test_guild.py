6e73cafd9d1773e4cbd788b1a16e283779be96e1
Eros <george.marr.cobc@gmail.com>
2018-09-19 10:23:00 +0100

    adds in guild test

--- tests/test_guild.py
+++ tests/test_guild.py
@@ -4,3 +4,10 @@ from constants import Setter
 
 def handle_setup(key):
     Setter.set_api_Key(key)
+
+
+def handle_player_test(uuid):
+    uuid = 'should-replace-with-my-key'
+    Guild.guid = uuid
+    print(Guild.get_guild_members())
+    print(Guild.JSON)
